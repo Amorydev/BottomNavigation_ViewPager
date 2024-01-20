ce480fff64f95d6382f7ea010fac398d96ad8b22
Amory.dev <amory.dev@gmail.com>
2024-01-20 10:23:26 +0700

    20/01/2024

--- app/src/main/java/com/example/bottomnavigation_viewpager/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation_viewpager/MainActivity.kt
@@ -10,10 +10,12 @@ import com.google.android.material.navigation.NavigationBarView
 
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
-    private val mOnNavigationSelectedLitener = NavigationBarView.OnItemSelectedListener {
+    //khởi tạo biến mOnNavigationSelectedListener để lắng nghe sự kiện khi kick vào viewpager
+    private val mOnNavigationSelectedListener = NavigationBarView.OnItemSelectedListener {
         item ->
         when(item.itemId)
         {
+            // khi item là menu 1 thí đặt con trỏ item là 0 và tương tự cho các menu còn lại
             R.id.menu_1 ->{
                 binding.viewPager.currentItem = 0
                 return@OnItemSelectedListener true
@@ -35,9 +37,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //gán giá trị cho viewAdapter bằng viewPagerAdapter
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager,lifecycle)
         binding.viewPager.adapter = viewPagerAdapter
-        binding.navigationBar.setOnItemSelectedListener(mOnNavigationSelectedLitener)
+        //gán trình lắng nghe vào navigationbar
+        binding.navigationBar.setOnItemSelectedListener(mOnNavigationSelectedListener)
+
+        //đăng kí trình lắng nghe cho viewPager
         binding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
